8ccbc1c334b0abdacb41f2f40d3303f9858f105b
Bradley Burgess <bradleyburgess@gmail.com>
2025-06-07 14:50:00 +0200

    feat(choirs): Add i18n to `BaseChoir` model

--- src/core/choirs/models.py
+++ src/core/choirs/models.py
@@ -1,14 +1,19 @@
 from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from core.base.models import BaseModel
 
 
 class BaseChoir(BaseModel):
-    name = models.CharField(max_length=80)
-    description = models.TextField(max_length=250)
+    name = models.CharField(_("name"), max_length=80)
+    description = models.TextField(_("description"), max_length=250)
     created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name=_("created by"),
     )
 
     def __str__(self):
