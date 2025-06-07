638a3499ceb15284ea6ef747a4878538fca02729
Bradley Burgess <bradleyburgess@gmail.com>
2025-06-07 12:52:24 +0200

    fix(choirs): Make `created_by` nullable`

--- src/core/choirs/models.py
+++ src/core/choirs/models.py
@@ -7,7 +7,9 @@ from core.base.models import BaseModel
 class BaseChoir(BaseModel):
     name = models.CharField(max_length=80)
     description = models.TextField(max_length=250)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True
+    )
 
     class Meta:  # type: ignore
         abstract = True
