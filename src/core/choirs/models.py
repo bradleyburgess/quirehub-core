db1cceeb7a32543793d12ea5178c20a426527e35
Bradley Burgess <bradleyburgess@gmail.com>
2025-06-07 12:32:21 +0200

    feat(choirs): Add `BaseChoir` model

---
+++ src/core/choirs/models.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.db import models
+
+from core.base.models import BaseModel
+
+
+class BaseChoir(BaseModel):
+    name = models.CharField(max_length=80)
+    description = models.TextField(max_length=250)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
+
+    class Meta:  # type: ignore
+        abstract = True
